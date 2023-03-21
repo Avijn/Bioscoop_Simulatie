a70817a8688c3a8836291fdbfaf0b1c5185da173
AVijn <arjan.witharen@gmail.com>
2023-03-21 20:01:29 +0100

    Room 0.1: created Room and Movie class
    Added some minor functions to the Room class

---
+++ Bioscoop_Simulatie/Bioscoop_Simulatie/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioscoop_Simulatie
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ Bioscoop_Simulatie/Bioscoop_Simulatie/Room.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bioscoop_Simulatie
+{
+    class Room
+    {
+        public string Name { get; set; }
+        public Movie movie { get; set; }
+        public Status Status { get; set; }
+        public int Seats { set; get; }
+        public int TakenSeats { get; set; }
+        public int CleanDuration { get; set; }
+        public Thread Thread;
+
+        public Room(string name, int seats, int cleanDuration)
+        {
+            Name = name;
+            Seats = seats;
+            Status = Status.Open;
+            CleanDuration = cleanDuration;
+            Thread = new Thread(Play);
+            Thread = new Thread(Clean);
+        }
+
+        /// <summary>
+        /// Adds a customer to the Room, this adds a takenseat unless the Takenseats equal the total amount of seats
+        /// </summary>
+        /// <returns>Returns TRUE if the customer can get a seat.<br /> Returns false if the room is full</returns>
+        public bool AddReservation()
+        {
+            if (Seats != TakenSeats)
+            {
+                TakenSeats = TakenSeats++;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// "Plays" the movie, waits the given duration of the movie <br />
+        /// Sets the TakenSeats to 0
+        /// </summary>
+        public void Play()
+        {
+            Status = Status.Playing;
+            TakenSeats = 0;
+            Thread.Sleep(movie.Duration);
+        }
+
+        /// <summary>
+        /// Starts cleaning the room, waits the given duration for cleaning
+        /// </summary>
+        public void Clean()
+        {
+            Status = Status.Cleaning;
+            Thread.Sleep(CleanDuration);
+        }
+    }
+
+    enum Status
+    {
+        Open,
+        Cleaning,
+        Playing
+    }
+}
