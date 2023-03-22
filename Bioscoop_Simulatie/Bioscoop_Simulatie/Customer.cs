6dfe8ffeb740d746b831418aca1382270b08e95b
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2023-03-22 13:51:02 +0100

    Ticket-system 0.1: Boilerplating
    Added default classes with properties

---
+++ Bioscoop_Simulatie/Bioscoop_Simulatie/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioscoop_Simulatie
+{
+    internal class Customer
+    {
+        public int Age { get; }
+        public Ticket Ticket { get; set; }
+
+        public Customer(int age)
+        {
+            Age = age;
+        }
+    }
+}
